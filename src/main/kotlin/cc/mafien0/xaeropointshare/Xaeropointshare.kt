91797f703c3ccaa1a0fa16e71e91b7aa877a160e
mafien0 <mafien_0@tuta.io>
2026-02-08 15:59:50 +0500

    readme, xaero's minimap dependency, `x%o`

--- src/main/kotlin/cc/mafien0/xaeropointshare/Xaeropointshare.kt
+++
@@ -1,9 +0,0 @@
-package cc.mafien0.xaeropointshare
-
-import net.fabricmc.api.ModInitializer
-
-class Xaeropointshare : ModInitializer {
-
-    override fun onInitialize() {
-    }
-}
--- src/main/kotlin/cc/mafien0/xaeropointshare/client/XaeropointshareClient.kt
+++
@@ -1,9 +0,0 @@
-package cc.mafien0.xaeropointshare.client
-
-import net.fabricmc.api.ClientModInitializer
-
-class XaeropointshareClient : ClientModInitializer {
-
-    override fun onInitializeClient() {
-    }
-}
